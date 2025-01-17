d082e540f79acacb5365bd1d24269f19c41a56b9
serediukit <serediukit@gmail.com>
2025-01-17 18:34:39 +0200

    add file

---
+++ 0XX/031.go
@@ -0,0 +1,28 @@
+package leetcode_ans
+
+func nextPermutation(nums []int) {
+	n := len(nums)
+
+	i := n - 2
+	for i >= 0 && nums[i] >= nums[i+1] {
+		i--
+	}
+
+	if i >= 0 {
+		j := n - 1
+		for nums[j] <= nums[i] {
+			j--
+		}
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+
+	reverse(nums, i+1, n-1)
+}
+
+func reverse(nums []int, start, end int) {
+	for start < end {
+		nums[start], nums[end] = nums[end], nums[start]
+		start++
+		end--
+	}
+}
