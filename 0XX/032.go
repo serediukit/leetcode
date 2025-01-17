6cbbe32f4fe7abd6b6ba737f34dff9a1dbe92a79
serediukit <serediukit@gmail.com>
2025-01-17 18:49:19 +0200

    add file

---
+++ 0XX/032.go
@@ -0,0 +1,29 @@
+package leetcode_ans
+
+func search(nums []int, target int) int {
+    start, end := 0, len(nums) - 1
+
+    for start <= end {
+        mid := (start + end) / 2
+
+        if nums[mid] == target {
+            return mid
+        }
+
+        if nums[start] <= nums[mid] {
+            if nums[start] <= target && target < nums[mid] {
+                end = mid - 1
+            } else {
+                start = mid + 1
+            }
+        } else {
+            if nums[mid] < target && target <= nums[end] {
+                start = mid + 1
+            } else {
+                end = mid - 1
+            }
+        }
+    }
+
+    return -1
+}
