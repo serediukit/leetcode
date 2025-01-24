ab91aaef2da26052dbe7bab2501f95fc06e2940c
serediukit <serediukit@gmail.com>
2025-01-24 23:42:38 +0200

    update solution

--- 0XX/093.go
+++ 0XX/093.go
@@ -7,33 +7,20 @@ func restoreIpAddresses(s string) []string {
 	}
 	var res []string
 	for i := 1; i <= min(3, ls-1); i++ {
-		s1 := s[:i]
-		if len(s1) > 1 && s1[0] == '0' {
-			continue
-		}
-		n1, _ := strconv.Atoi(s1)
+		n1, _ := strconv.Atoi(s[:i])
 		if n1 >= 0 && n1 < 256 {
 			for j := i + 1; j <= min(i+3, ls-1); j++ {
-				s2 := s[i:j]
-				if len(s2) > 1 && s2[0] == '0' {
-					continue
-				}
-				n2, _ := strconv.Atoi(s2)
+				n2, _ := strconv.Atoi(s[i:j])
 				if n2 >= 0 && n2 < 256 {
 					for k := j + 1; k <= min(j+3, ls-1); k++ {
-						s3 := s[j:k]
-						if len(s3) > 1 && s3[0] == '0' {
-							continue
-						}
-						n3, _ := strconv.Atoi(s3)
+						n3, _ := strconv.Atoi(s[j:k])
 						if n3 >= 0 && n3 < 256 {
-							s4 := s[k:ls]
-							if len(s4) > 1 && s4[0] == '0' {
-								continue
-							}
-							n4, _ := strconv.Atoi(s4)
+							n4, _ := strconv.Atoi(s[k:ls])
 							if n4 >= 0 && n4 < 256 {
-								res = append(res, fmt.Sprintf("%d.%d.%d.%d", n1, n2, n3, n4))
+								r := fmt.Sprintf("%d.%d.%d.%d", n1, n2, n3, n4)
+								if len(r) == len(s) + 3 {
+									res = append(res, r)
+								}
 							}
 						}
 					}
