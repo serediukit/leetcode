582c91c277d45e777b9562f80c5e47a42e93bc40
serediukit <serediukit@gmail.com>
2025-02-01 23:20:36 +0200

    update solution

--- 0XX/056.go
+++ 0XX/056.go
@@ -9,30 +9,15 @@ func merge(intervals [][]int) [][]int {
 		return (intervals[a][0] < intervals[b][0]) || ((intervals[a][0] == intervals[b][0]) && (intervals[a][1] < intervals[b][1]))
 	})
 
+	res := [][]int{intervals[0]}
+
 	for i := 1; i < len(intervals); i++ {
-		mergedArray, hasMerge := mergeTwoIntervals(intervals[i-1], intervals[i])
-		if hasMerge {
-			intervals[i-1] = mergedArray
-			intervals = append(intervals[:i], intervals[i+1:]...)
-			i--
+		if res[len(res)-1][1] >= intervals[i][0] {
+			res[len(res)-1][1] = max(res[len(res)-1][1], intervals[i][1])
+		} else {
+			res = append(res, intervals[i])
 		}
 	}
 
-	return intervals
-}
-
-func mergeTwoIntervals(a []int, b []int) ([]int, bool) {
-	if a[0] <= b[0] && a[1] >= b[0] {
-		if a[1] >= b[1] {
-			return a, true
-		}
-		return []int{a[0], b[1]}, true
-	} else if b[0] <= a[0] && b[1] >= a[0] {
-		if b[1] >= a[1] {
-			return b, true
-		}
-		return []int{b[0], a[1]}, true
-	}
-
-	return nil, false
+	return res
 }
