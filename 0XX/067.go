e8524791ef97367bc9f5b54fcfa4df4c76384518
serediukit <serediukit@gmail.com>
2025-02-03 22:01:35 +0200

    update solution

--- 0XX/067.go
+++ 0XX/067.go
@@ -1,38 +1,40 @@
 package leetcode_ans
 
 func addBinary(a string, b string) string {
-	res := ""
+	res := []rune{}
 
-	a = Reverse(a)
-	b = Reverse(b)
+    A := []rune(a)
+    B := []rune(b)
+	slices.Reverse(A)
+    slices.Reverse(B)
 
 	l := max(len(a), len(b))
 
-	var ost byte = '0'
-	var sum string
+	var ost rune = '0'
+	var sum rune = '0'
 	for i := 0; i < l; i++ {
-		if i < len(a) {
-			if i < len(b) {
-				sum, ost = sumBin(a[i], b[i], ost)
-				res = sum + res
+		if i < len(A) {
+			if i < len(B) {
+				sum, ost = sumBin(A[i], B[i], ost)
 			} else {
-				sum, ost = sumBin(a[i], '0', ost)
-				res = sum + res
+				sum, ost = sumBin(A[i], '0', ost)
 			}
-		} else if i < len(b) {
-			sum, ost = sumBin('0', b[i], ost)
-			res = sum + res
+		} else if i < len(B) {
+			sum, ost = sumBin('0', B[i], ost)
 		}
+        res = append(res, sum)
 	}
 
 	if ost == '1' {
-		res = "1" + res
+		res = append(res, '1')
 	}
 
-	return res
+    slices.Reverse(res)
+
+	return string(res)
 }
 
-func sumBin(a byte, b byte, ost byte) (string, byte) {
+func sumBin(a rune, b rune, ost rune) (rune, rune) {
 	n1 := a - 48
 	n2 := b - 48
 	n3 := ost - 48
@@ -42,12 +44,5 @@ func sumBin(a byte, b byte, ost byte) (string, byte) {
 	r1 := (sum % 2) + 48
 	r2 := (sum / 2) + 48
 
-	return string(r1), r2
-}
-
-func Reverse(in string) (out string) {
-	for _, r := range in {
-		out = string(r) + out
-	}
-	return
+	return r1, r2
 }
