1c9e097c143232fea88afd59ed2fe9b84ba095ec
serediukit <serediukit@gmail.com>
2025-02-12 22:13:29 +0200

    update solution

--- 0XX/073.go
+++ 0XX/073.go
@@ -31,16 +31,8 @@ func setZeroes(matrix [][]int)  {
     }
 
     for i := 1; i < m; i++ {
-        if matrix[i][0] == 0 {
-            for j := 1; j < n; j++ {
-                matrix[i][j] = 0
-            }
-        }
-    }
-
-    for j := 1; j < n; j++ {
-        if matrix[0][j] == 0 {
-            for i := 1; i < m; i++ {
+        for j := 1; j < n; j++ {
+            if matrix[i][0] == 0 || matrix[0][j] == 0 {
                 matrix[i][j] = 0
             }
         }
