cfeb50fe4eb34484e4ebf3c9a45214d23446d9f8
serediukit <serediukit@gmail.com>
2025-02-12 22:13:16 +0200

    update solution

--- 0XX/073.go
+++ 0XX/073.go
@@ -4,23 +4,57 @@ func setZeroes(matrix [][]int)  {
     m := len(matrix)
     n := len(matrix[0])
 
-    rows := make([]bool, m)
-    cols := make([]bool, n)
+    var hasFirstRowZero bool
+    var hasFirstColZero bool
 
-    for i := 0; i < m; i++ {
-        for j := 0; j < n; j++ {
-            if matrix[i][j] == 0 {
-                rows[i] = true
-                cols[j] = true
-            }
+    for j := 0; j < n; j++ {
+        if matrix[0][j] == 0 {
+            hasFirstRowZero = true
+            break
         }
     }
 
     for i := 0; i < m; i++ {
-        for j := 0; j < n; j++ {
-            if rows[i] || cols[j] {
+        if matrix[i][0] == 0 {
+            hasFirstColZero = true
+            break
+        }
+    }
+
+    for i := 1; i < m; i++ {
+        for j := 1; j < n; j++ {
+            if matrix[i][j] == 0 {
+                matrix[i][0] = 0
+                matrix[0][j] = 0
+            }
+        }
+    }
+
+    for i := 1; i < m; i++ {
+        if matrix[i][0] == 0 {
+            for j := 1; j < n; j++ {
                 matrix[i][j] = 0
             }
+        }
+    }
+
+    for j := 1; j < n; j++ {
+        if matrix[0][j] == 0 {
+            for i := 1; i < m; i++ {
+                matrix[i][j] = 0
+            }
+        }
+    }
+
+    if hasFirstRowZero {
+        for j := 0; j < n; j++ {
+            matrix[0][j] = 0
+        }
+    }
+
+    if hasFirstColZero {
+        for i := 0; i < m; i++ {
+            matrix[i][0] = 0
         }
     }
 }
