ebba4c8cd8bd7318186a3c16d9556b691a767644
serediukit <serediukit@gmail.com>
2025-03-13 23:38:41 +0200

    add file

---
+++ 33XX/3356.go
@@ -0,0 +1,29 @@
+package leetcode_ans
+
+func minZeroArray(nums []int, queries [][]int) int {
+    cntZeroes := 0
+    for _, n := range nums {
+        if n == 0 {
+            cntZeroes++
+        }
+    }
+    k := 0
+    for _, q := range queries {
+        if cntZeroes < len(nums) {
+            for i := q[0]; i <= q[1]; i++ {
+                if nums[i] != 0 {
+                    if nums[i] <= q[2] {
+                        nums[i] = 0
+                        cntZeroes++
+                    } else {
+                        nums[i] -= q[2]
+                    }
+                }
+            }
+        } else {
+            return k
+        }
+        k++
+    }
+    return -1
+}
