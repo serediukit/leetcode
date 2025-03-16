c2a0b7c5166b0d4be44d54c9cfd7011c5dd31e9d
serediukit <serediukit@gmail.com>
2025-03-16 23:47:45 +0200

    update solution

--- 25XX/2594.go
+++ 25XX/2594.go
@@ -2,20 +2,25 @@ package leetcode_ans
 
 func repairCars(ranks []int, cars int) int64 {
     var left, right int64 = 0, int64(slices.Max(ranks)) * int64(cars) * int64(cars)
-    for left < right {
-        mid := (left + right) / 2
 
+    canRepair := func(minutes int64) bool {
         countRepair := 0
 
         for _, r := range ranks {
-            countRepair += int(math.Sqrt(float64(mid) / float64(r)))
+            countRepair += int(math.Sqrt(float64(minutes / int64(r))))
 
             if countRepair >= cars {
-                break
+                return true
             }
         }
 
-        if countRepair >= cars {
+        return false
+    }
+
+    for left < right {
+        mid := (left + right) / 2
+
+        if canRepair(mid) {
             right = mid
         } else {
             left = mid + 1
