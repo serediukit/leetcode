9d20741ee446030b17e62039cf714d75fba0c361
serediukit <serediukit@gmail.com>
2025-03-17 23:06:07 +0200

    update solution

--- 22XX/2206.go
+++ 22XX/2206.go
@@ -1,13 +1,14 @@
 package leetcode_ans
 
 func divideArray(nums []int) bool {
-	pairs := make(map[int]bool)
+	pairs := make(map[int]int)
+
 	for _, n := range nums {
-		pairs[n] = !pairs[n]
+		pairs[n]++
 	}
 
 	for _, p := range pairs {
-		if p {
+		if p % 2 != 0 {
 			return false
 		}
 	}
