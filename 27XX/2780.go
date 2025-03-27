c2b1e69a1d51f8ec7b6f5ba516c9c4b886e72281
serediukit <serediukit@gmail.com>
2025-03-27 23:51:46 +0200

    update solution

--- 27XX/2780.go
+++ 27XX/2780.go
@@ -1,21 +1,40 @@
 package leetcode_ans
 
 func minimumIndex(nums []int) int {
-    countRight := make(map[int]int, 0)
-    countLeft := make(map[int]int, 0)
-
-    for _, num := range nums {
-        countRight[num]++
-    }
-
+    dominant := dominantNum(slices.Clone(nums))
+    countLeft := 0
+    countRight := count(nums, dominant)
+    left := 0
+    right := len(nums)
+    
     for i, num := range nums {
-        countLeft[num]++
-        countRight[num]--
+        left++
+        right--
 
-        if countLeft[num] > (i + 1) / 2 && countRight[num] > (len(nums) - i - 1) / 2 {
+        if num == dominant {
+            countLeft++
+            countRight--
+        }
+
+        if 2 * countLeft > left && 2 * countRight > right {
             return i
         }
     }
 
     return -1
+}
+
+func dominantNum(nums []int) int {
+    sort.Ints(nums)
+    return nums[len(nums)/2]
+}
+
+func count(nums []int, n int) int {
+    count := 0
+    for _, num := range nums {
+        if num == n {
+            count++
+        }
+    }
+    return count
 }
