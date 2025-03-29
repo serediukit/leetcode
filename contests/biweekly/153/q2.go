dc240d834dfd2b4930339b72b2ba66276d334824
serediukit <serediukit@gmail.com>
2025-03-29 17:31:03 +0200

    update solution

--- contests/biweekly/153/q2.go
+++ contests/biweekly/153/q2.go
@@ -1,49 +1,39 @@
 package biweekly
 
 func maxActiveSectionsAfterTrade(s string) int {
-	s = "1" + s + "1"
-	cMax := strings.Count(s, "1")
+	n := len(s)
+	maxC := strings.Count(s, "1")
 
-	count := make([]int, 0)
-
-	c := 1
-	for i := 1; i < len(s); i++ {
-		if s[i] == s[i-1] {
-			c++
+	ans := 0
+	i := 0
+	for i < n {
+		if s[i] == '1' {
+			start := i
+			for i < n && s[i] == '1' {
+				i++
+			}
+			end := i - 1
+			if start > 0 && end < n-1 {
+				leftZeros := 0
+				j := start - 1
+				for j >= 0 && s[j] == '0' {
+					leftZeros++
+					j--
+				}
+				rightZeros := 0
+				j = end + 1
+				for j < n && s[j] == '0' {
+					rightZeros++
+					j++
+				}
+				more := leftZeros + rightZeros
+				if more > ans {
+					ans = more
+				}
+			}
 		} else {
-			count = append(count, c)
-			c = 1
+			i++
 		}
 	}
-	count = append(count, c)
-
-	if len(count) < 5 {
-		return cMax - 2
-	}
-
-	for i := 2; i < len(count)-2; i += 2 {
-		mergedZeroes := make([]int, 0, len(count)-2)
-		mergedZeroes = append(mergedZeroes, count[:i-1]...)
-		mergedZeroes = append(mergedZeroes, count[i-1]+count[i]+count[i+1])
-		mergedZeroes = append(mergedZeroes, count[i+2:]...)
-
-		for j := 1; j < len(mergedZeroes)-1; j += 2 {
-			mergedOnes := make([]int, 0, len(mergedZeroes)-2)
-			mergedOnes = append(mergedOnes, mergedZeroes[:j-1]...)
-			mergedOnes = append(mergedOnes, mergedZeroes[j-1]+mergedZeroes[j]+mergedZeroes[j+1])
-			if j < len(mergedZeroes)-2 {
-				mergedOnes = append(mergedOnes, mergedZeroes[j+2:]...)
-			}
-
-			sum := 0
-			for k := 0; k < len(mergedOnes); k += 2 {
-				sum += mergedOnes[k]
-			}
-			if sum > cMax {
-				cMax = sum
-			}
-		}
-	}
-
-	return cMax - 2
-}
+	return maxC + ans
+}©leetcode
